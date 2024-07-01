43fa3bdfb20fcb17271ca5a5e69a91da16b8280e
Vladimir Fibe <motiw@icloud.com>
2024-07-01 15:40:13 +0500

    Create TrendingCell

---
+++ Recipes/TrendingCell.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class TrendingCell: UICollectionViewCell {
+    
+}
--- Recipes/ViewController.swift
+++ Recipes/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let cell = TrendingCell()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCell()
     }
 
-
+    private func setupCell() {
+        view.addSubview(cell)
+        cell.backgroundColor = .red
+        cell.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cell.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            cell.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            cell.widthAnchor.constraint(equalToConstant: 300),
+            cell.heightAnchor.constraint(equalToConstant: 150)
+        
+        ])
+    }
 }
 
+@available(iOS 17.0, *)
+#Preview {
+    ViewController()
+}
