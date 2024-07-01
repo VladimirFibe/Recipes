6d2e74b5f3f4acc25de5ad5791ff2746f09ebc99
Valentina Popova <info@valentina-popova.ru>
2024-07-01 22:18:28 +0300

    Add CreatorCell

---
+++ Recipes/CreatorCell.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+class CreatorCell: UICollectionViewCell {
+    private let avatarImageView = UIImageView()
+    private let nameLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupAvatarImageView()
+        setupNameLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupAvatarImageView() {
+        contentView.addSubview(avatarImageView)
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        avatarImageView.layer.cornerRadius = 40
+        avatarImageView.layer.masksToBounds = true
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 80),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor)
+            
+        ])
+    }
+    
+    private func setupNameLabel() {
+        contentView.addSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.textAlignment = .center
+        nameLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+    
+    func configure(with image: UIImage, name: String) {
+        avatarImageView.image = image
+        nameLabel.text = name
+    }
+}
