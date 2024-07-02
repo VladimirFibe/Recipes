b866aa070ec2ee72ab561105f11caad3e593b95d
Vladimir Fibe <motiw@icloud.com>
2024-07-02 10:19:02 +0500

    RecentCell

---
+++ Recipes/BookmarkView.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+class BookmarkView: UIView {
+    private let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        backgroundColor = .white
+        layer.cornerRadius = 16
+        layer.masksToBounds = true
+    }
+    
+    private func setupImageView() {
+        addSubview(imageView)
+        imageView.image = .bookmarkInactive24
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+           imageView.widthAnchor.constraint(equalToConstant: 24),
+           imageView.heightAnchor.constraint(equalTo: widthAnchor),
+           widthAnchor.constraint(equalToConstant: 32),
+           heightAnchor.constraint(equalTo: widthAnchor),
+           imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+           imageView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    func configure(with active: Bool) {
+        imageView.image = active ? .bookmarkActive24 : .bookmarkInactive24
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    ViewController()
+}
