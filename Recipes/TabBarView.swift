8b922a45f35073499e37e60c9b0562f00682cdfa
Dmitriy <snipere@list.ru>
2024-07-02 19:46:41 +1000

    Add plus button

--- Recipes/TabBarView.swift
+++ Recipes/TabBarView.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+/// Протокол делегата
+protocol ITabBarViewDelegate: AnyObject {
+	
+	/// Добавить
+	func add()
+}
+
 final class TabBarView: UIView {
 	
 	// MARK: - Outlets
@@ -14,10 +21,13 @@ final class TabBarView: UIView {
 	// MARK: - Public properties
 	
 	// MARK: - Dependencies
-	
+
+	weak var delegate: ITabBarViewDelegate?
+
 	// MARK: - Private properties
 
 	private lazy var shapeLayer = makeShapeLayer()
+	private lazy var plusButton = makeButton()
 
 	// MARK: - Initialization
 
@@ -45,7 +55,11 @@ final class TabBarView: UIView {
 // MARK: - Actions
 
 private extension TabBarView {
-	
+
+	@objc
+	func plusButtonTapped(_ sender: UIButton) {
+		delegate?.add()
+	}
 }
 
 // MARK: - Setup UI
@@ -64,11 +78,25 @@ private extension TabBarView {
 		let layer = CAShapeLayer()
 
 		layer.fillColor = UIColor.white.cgColor
-		layer.shadowColor = UIColor(red: 108 / 255, green: 108 / 255, blue: 108 / 255, alpha: 0.08).cgColor
+		layer.shadowColor = UIColor(red: 0.42, green: 0.42, blue: 0.42, alpha: 0.08).cgColor
 		layer.shadowOpacity = 0.8
 		layer.shadowOffset = CGSize(width: 0, height: -1)
 		
 		return layer
+	}
+
+	func makeButton() -> UIButton {
+		let element = UIButton()
+
+		element.configuration = .filled()
+		element.configuration?.baseBackgroundColor = UIColor(red: 0.89, green: 0.24, blue: 0.24, alpha: 1.00)
+		element.configuration?.cornerStyle = .capsule
+		element.configuration?.image = UIImage(named: "plus")?.withRenderingMode(.alwaysTemplate)
+		element.configuration?.baseForegroundColor = .black
+		element.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
+		element.translatesAutoresizingMaskIntoConstraints = false
+
+		return element
 	}
 }
 
@@ -77,7 +105,7 @@ private extension TabBarView {
 private extension TabBarView {
 	
 	func addSubviews() {
-		
+		addSubview(plusButton)
 	}
 }
 
@@ -89,7 +117,10 @@ private extension TabBarView {
 		setupShapeLayerPath()
 
 		NSLayoutConstraint.activate([
-			
+			plusButton.topAnchor.constraint(equalTo: topAnchor),
+			plusButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+			plusButton.widthAnchor.constraint(equalToConstant: 48),
+			plusButton.heightAnchor.constraint(equalToConstant: 48),
 		])
 	}
 
@@ -100,14 +131,14 @@ private extension TabBarView {
 		let height = frame.height
 		
 		path.move(to: CGPoint(x: 0, y: 14))
-		path.addLine(to: CGPoint(x: centerWidth - 48, y: 14))
+		path.addLine(to: CGPoint(x: centerWidth - 58, y: 14))
 		path.addCurve(
 			to: CGPoint(x: centerWidth, y: 58),
 			controlPoint1: CGPoint(x: centerWidth - 28, y: 14),
 			controlPoint2: CGPoint(x: centerWidth - 38, y: 58)
 		)
 		path.addCurve(
-			to: CGPoint(x: centerWidth + 48, y: 14),
+			to: CGPoint(x: centerWidth + 58, y: 14),
 			controlPoint1: CGPoint(x: centerWidth + 38, y: 58),
 			controlPoint2: CGPoint(x: centerWidth + 28, y: 14)
 		)
