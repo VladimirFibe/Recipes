61afc6ce85b191ff36d7910fdabb55390b835c51
Vladimir Fibe <motiw@icloud.com>
2024-07-07 02:38:45 +0500

    setupCollectionView

---
+++ Recipes/Modules/Home/HomeSection.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum HomeSection {
+    case trending
+    case popular
+    case recent
+}
--- Recipes/Modules/Home/HomeViewController.swift
+++ Recipes/Modules/Home/HomeViewController.swift
@@ -1,9 +1,25 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        setupCollectionView()
+    }
+}
+//MARK: - Setup Views
+private extension HomeViewController {
+    func setupCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .blue
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
 @available(iOS 17.0, *)
