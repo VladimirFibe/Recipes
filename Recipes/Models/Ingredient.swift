ed356d65e4c9ebda6ac4f2830fa951c8a0234fe1
Vladimir Fibe <motiw@icloud.com>
2024-07-13 09:04:37 +0500

    fix

--- Recipes/Models/Ingredient.swift
+++ Recipes/Models/Ingredient.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 struct Ingredient: Codable, Hashable {
-    let id: Int
-    let image: String?
-    let name: String
-    let amount: Double
-    let unit: String
-
+    var id: Int = 0
+    var image: String?
+    var name: String
+    var amount: Double = 0
+    var unit: String = ""
+    var quantity: String? = ""
     var sourceImage: String? {
         guard let image else { return nil }
         return "https://spoonacular.com/cdn/ingredients_100x100/\(image)"
