9c564613a6849e94163b56d102df3abfe9883eba
Vladimir Fibe <motiw@icloud.com>
2024-07-14 13:06:19 +0500

    fix

--- Recipes/Modules/RecipeDetail/RecipeDetailViewController.swift
+++ Recipes/Modules/RecipeDetail/RecipeDetailViewController.swift
@@ -140,6 +140,10 @@ final class RecipeDetailViewController: UIViewController {
     """
     <!DOCTYPE html>
     <html><head>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no'>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <style> body {
     font-family: "Poppins", sans-serif;
     font-weight: 400;
